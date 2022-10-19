fc9b8ff551a1359d6722848ea5f2ad20a90c1f63
Steven Shireff <sa.shireff2914@gmaill.com>
2022-10-19 18:53:41 -0400

    commit tree 7208e837041656472e884848aa81b937a3a655b6

---
+++ index.js
@@ -0,0 +1,45 @@
+function add() {
+
+}
+function subtract() {
+
+}
+
+function multiply() {
+
+}
+
+function divide() {
+
+}
+function add(a,b) {
+    return a + b;
+}
+function subtract(a,b) {
+    return a - b; 
+}
+function multiply(a,b) {
+    return a * b;
+}
+function divide(a,b) {
+    return a/b;
+}
+
+let number = 10
+function increment(number){
+    return (number += 1);    
+  }
+
+let numbers = 99
+function decrement(numbers){
+    return(numbers -+ 1);
+}
+
+let top = "456"
+function makeInt(top){
+    return(parseInt(top,10));
+}
+let drop = "345.6789"
+function preserveDecimal(drop){
+    return(parseFloat(drop))
+}
